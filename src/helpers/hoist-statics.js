c4bbc7ad111f14097070041c79a5d328cc91f961
Justin Ridgewell <justin@ridgewell.name>
2015-10-01 11:56:57 -0400

    Extract hoist logic from each loop

--- src/helpers/hoist-statics.js
+++ src/helpers/hoist-statics.js
@@ -1,5 +1,40 @@
 import replaceArrow from "./replace-arrow";
 
+function hoist(t, path, binding, hoisted, elements) {
+  const parent = (binding) ? binding.path.parentPath : path.parentPath;
+  if (parent.isArrowFunctionExpression()) {
+    // The key variable is a formal parameter
+    const body = parent.get("body");
+
+    if (path.parentPath === parent) {
+      // If the parameter in the parent function, then we need to place the
+      // assignment into the iDOM element calls that we replace the arrow
+      // function with.
+      elements.unshift(hoisted);
+    } else if (body.isBlockStatement()) {
+      // This one's easy, just push the assignment to the top of the
+      // function.
+      body.unshiftContainer("body", hoisted);
+    } else {
+      // And this one's a pain. We have to replace the entire arrow
+      // function with the assignment and an explicit return for the
+      // implicitly returned expression.
+      replaceArrow(t, parent, [ hoisted, t.returnStatement(body.node) ]);
+    }
+  } else if (parent.isFunction()) {
+    // The key variable is a formal parameter
+    parent.get("body").unshiftContainer("body", hoisted);
+  } else if (binding && binding.constant) {
+    // With a key variable, we should update they key's value
+    // in the statics array whenever the variable is updated.
+    const statement = binding.path.getStatementParent();
+    statement.insertAfter(hoisted);
+  } else {
+    // Some rouge global key variable
+    elements.unshift(hoisted);
+  }
+}
+
 export default function hoistStatics(t, scope, path, staticAttrs, elements, { eager }) {
   staticAttrs.forEach((attrs) => {
     const declaration = t.variableDeclaration("let", [attrs.declarator]);
@@ -25,38 +60,7 @@ export default function hoistStatics(t, scope, path, staticAttrs, elements, { ea
         ));
       }
 
-      const parent = (binding) ? binding.path.parentPath : path.parentPath;
-      if (parent.isArrowFunctionExpression()) {
-        // The key variable is a formal parameter
-        const body = parent.get("body");
-
-        if (path.parentPath === parent) {
-          // If the parameter in the parent function, then we need to place the
-          // assignment into the iDOM element calls that we replace the arrow
-          // function with.
-          elements.unshift(hoisted);
-        } else if (body.isBlockStatement()) {
-          // This one's easy, just push the assignment to the top of the
-          // function.
-          body.unshiftContainer("body", hoisted);
-        } else {
-          // And this one's a pain. We have to replace the entire arrow
-          // function with the assignment and an explicit return for the
-          // implicitly returned expression.
-          replaceArrow(t, parent, [ hoisted, t.returnStatement(body.node) ]);
-        }
-      } else if (parent.isFunction()) {
-        // The key variable is a formal parameter
-        parent.get("body").unshiftContainer("body", hoisted);
-      } else if (binding && binding.constant) {
-        // With a key variable, we should update they key's value
-        // in the statics array whenever the variable is updated.
-        const statement = binding.path.getStatementParent();
-        statement.insertAfter(hoisted);
-      } else {
-        // Some rouge global key variable
-        elements.unshift(hoisted);
-      }
+      hoist(t, path, binding, hoisted, elements);
     }
 
     // If we're not wrapping the JSX Element, put the statics array into
@@ -67,3 +71,5 @@ export default function hoistStatics(t, scope, path, staticAttrs, elements, { ea
     }
   });
 }
+
+

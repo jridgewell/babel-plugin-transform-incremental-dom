062235893998ec17d49007985f68557f13b18493
Justin Ridgewell <justin@ridgewell.name>
2015-12-13 19:23:30 -0500

    Test that spread attributes are closed over

--- test/fixtures/spread-attributes/actual.js
+++ test/fixtures/spread-attributes/actual.js
@@ -1,4 +1,5 @@
 function render() {
+  var test = <div {...props} />;
   return (<div class="test" id={id} {...props} key="test" data-expanded={expanded} {...props.attrs}>
     <div {...props} />
   </div>);
